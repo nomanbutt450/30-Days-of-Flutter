71e7a76d2e293b5c514bf59687cf83c7930b2bd6
Noman <nomanbutt450@gmail.com>
2021-07-30 21:25:00 +0500

    Chapter 2 Learn Scaffold basic & DataTypes & GitHub

---
+++ lib/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //Data Types of Flutter///
+    int days = 30;
+    double day = 2.5; //use for floating
+    String name = "Noman";
+    bool isMale = true;
+    num temp; //take both float and int
+    var today = "Friday";
+    const pie = 3.14;
+    final price; //list ma values ko modify kr skta ha
+
+    ////Scafold k andr head,body & Footer hota ha//////////
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+          child: Container(
+              child: Text("Welcome to $days days of flutter by $name"))),
+      drawer: Drawer(),
+    );
+  }
+}
